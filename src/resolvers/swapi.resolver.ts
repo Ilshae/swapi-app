6090997a0fec8213b330810b6a7693148bbdaddd
Ilshae <pruszynskaingrid@outlook.com>
2024-12-04 19:47:33 +0100

    Fix missing return types in resolver

--- src/resolvers/swapi.resolver.ts
+++ src/resolvers/swapi.resolver.ts
@@ -11,12 +11,12 @@ export class SwapiResolver {
   async films(
     @Args('page', { nullable: true }) page?: number,
     @Args('filter', { nullable: true }) filter?: string,
-  ) {
+  ): Promise<Film[]> {
     return this.swapiService.fetchResource('films', page, filter);
   }
 
   @Query(() => Film)
-  async film(@Args('id') id: number) {
+  async film(@Args('id') id: number): Promise<Film> {
     return this.swapiService.fetchResourceById('films', id);
   }
 
@@ -24,12 +24,12 @@ export class SwapiResolver {
   async species(
     @Args('page', { nullable: true }) page?: number,
     @Args('filter', { nullable: true }) filter?: string,
-  ) {
+  ): Promise<Species[]> {
     return this.swapiService.fetchResource('species', page, filter);
   }
 
   @Query(() => Species)
-  async speciesById(@Args('id') id: number) {
+  async speciesById(@Args('id') id: number): Promise<Species> {
     return this.swapiService.fetchResourceById('species', id);
   }
 
@@ -37,12 +37,12 @@ export class SwapiResolver {
   async vehicles(
     @Args('page', { nullable: true }) page?: number,
     @Args('filter', { nullable: true }) filter?: string,
-  ) {
+  ): Promise<Vehicle[]> {
     return this.swapiService.fetchResource('vehicles', page, filter);
   }
 
   @Query(() => Vehicle)
-  async vehicle(@Args('id') id: number) {
+  async vehicle(@Args('id') id: number): Promise<Vehicle> {
     return this.swapiService.fetchResourceById('vehicles', id);
   }
 
@@ -50,12 +50,12 @@ export class SwapiResolver {
   async starships(
     @Args('page', { nullable: true }) page?: number,
     @Args('filter', { nullable: true }) filter?: string,
-  ) {
+  ): Promise<Starship[]> {
     return this.swapiService.fetchResource('starships', page, filter);
   }
 
   @Query(() => Starship)
-  async starship(@Args('id') id: number) {
+  async starship(@Args('id') id: number): Promise<Starship> {
     return this.swapiService.fetchResourceById('starships', id);
   }
 
@@ -63,12 +63,12 @@ export class SwapiResolver {
   async planets(
     @Args('page', { nullable: true }) page?: number,
     @Args('filter', { nullable: true }) filter?: string,
-  ) {
+  ): Promise<Planet[]> {
     return this.swapiService.fetchResource('planets', page, filter);
   }
 
   @Query(() => Planet)
-  async planet(@Args('id') id: number) {
+  async planet(@Args('id') id: number): Promise<Planet> {
     return this.swapiService.fetchResourceById('planets', id);
   }
 }
